f22bce3958746f9d613c7186fe2e186e5611d7a2
Yosvanni <yosvannima@gmail.com>
2025-11-11 16:12:23 -0500

    Docker configuration.

--- TMTestApp.Server/Program.cs
+++ TMTestApp.Server/Program.cs
@@ -2,6 +2,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// CORS configuration for development
+var cors = "_dev";
+builder.Services.AddCors(o =>
+{
+    o.AddPolicy(cors, p => p
+        .WithOrigins("http://localhost:5173")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials());
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -9,22 +20,24 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    
+    // Auto-migration in Development (uncomment if you have EF Core and AppDbContext):
+    // using var scope = app.Services.CreateScope();
+    // var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    // db.Database.Migrate();
 }
+
+app.UseCors(cors);
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.MapFallbackToFile("/index.html");
 
 app.Run();
